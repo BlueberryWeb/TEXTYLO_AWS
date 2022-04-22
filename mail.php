be27225b852a6453b2827cd998d61e1c80078117
BlueberryWeb <101658724+BlueberryWeb@users.noreply.github.com>
2022-04-22 12:27:45 -0500

    .

--- mail.php
+++ mail.php
@@ -1,5 +1,20 @@
-<?php
 
+<?php
+/**
+ * Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * http://aws.amazon.com/apache2.0/
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+// Import PHPMailer classes into the global namespace.
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -7,16 +22,101 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
-$nombre = trim($_POST['nombre']);
-$email = trim($_POST['email']);
-$telefono = trim($_POST['telefono']);
-$mensaje = trim($_POST['mensaje']);
+// Replace sender@example.com with your "From" address.
+// This address must be verified with Amazon SES.
+$sender = 'sender@example.com';
+$senderName = 'Sender Name';
 
-$message = file_get_contents('correo.php');
-$message = str_replace('%nombre%', $nombre, $message);
-$message = str_replace('%email%', $email, $message);
-$message = str_replace('%telefono%', $telefono, $message);
-$message = str_replace('%mensaje%', $mensaje, $message);
+// Replace recipient@example.com with a "To" address. If your account
+// is still in the sandbox, this address must be verified.
+$recipient = 'recipient@example.com';
+
+// Replace smtp_username with your Amazon SES SMTP user name.
+$usernameSmtp = 'AKIARCABGFNVR6EAYP44';
+
+// Replace smtp_password with your Amazon SES SMTP password.
+$passwordSmtp = 'BG1ZoW3ce7roxDNPx4EFTn2/WsH4+0HNgkgbYuhXOb8l';
+
+// Specify a configuration set. If you do not want to use a configuration
+// set, comment or remove the next line.
+$configurationSet = 'ConfigSet';
+
+// If you're using Amazon SES in a region other than US West (Oregon),
+// replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP
+// endpoint in the appropriate region.
+$host = 'email-smtp.us-east-1.amazonaws.com';
+$port = 587;
+
+// The subject line of the email
+$subject = 'Amazon SES test (SMTP interface accessed using PHP)';
+
+// The plain-text body of the email
+$bodyText =  "Email Test\r\nThis email was sent through the
+    Amazon SES SMTP interface using the PHPMailer class.";
+
+// The HTML-formatted body of the email
+$bodyHtml = '<h1>Email Test</h1>
+    <p>This email was sent through the
+    <a href="https://aws.amazon.com/ses">Amazon SES</a> SMTP
+    interface using the <a href="https://github.com/PHPMailer/PHPMailer">
+    PHPMailer</a> class.</p>';
+
+$mail = new PHPMailer(true); 
+
+try {
+    // Specify the SMTP settings.
+    $mail->isSMTP();
+    $mail->setFrom($sender, $senderName);
+    $mail->Username   = $usernameSmtp;
+    $mail->Password   = $passwordSmtp;
+    $mail->Host       = $host;
+    $mail->Port       = $port;
+    $mail->SMTPAuth   = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->addCustomHeader('X-SES-CONFIGURATION-SET', $configurationSet);
+
+    // Specify the message recipients.
+    $mail->addAddress($recipient);
+    // You can also add CC, BCC, and additional To recipients here.
+
+    // Specify the content of the message.
+    $mail->isHTML(true);
+    $mail->Subject    = $subject;
+    $mail->Body       = $bodyHtml;
+    $mail->AltBody    = $bodyText;
+    $mail->Send();
+    echo "Email sent!" , PHP_EOL;
+} catch (phpmailerException $e) {
+    echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
+} catch (Exception $e) {
+    echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
 
 $mail = new PHPMailer;
 $mail->isSMTP();
@@ -44,4 +144,4 @@ if(!$mail->send()) {
   echo "Mensaje enviado";
 //   header("Location: {$_SERVER['HTTP_REFERER']}"); 
 exit;
-}
+} -->
