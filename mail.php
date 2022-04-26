1a5a7bb76639da191973ec955d39b698ddca593e
BlueberryWeb <101658724+BlueberryWeb@users.noreply.github.com>
2022-04-26 08:49:39 -0500

    correo 2.0

--- mail.php
+++ mail.php
@@ -15,10 +15,10 @@
  */
 
 
-  $nombre = trim($_POST['nombre']);
-  $email = trim($_POST['email']);
-  $telefono = trim($_POST['telefono']);
-  $mensaje = trim($_POST['mensaje']);
+$nombre = trim($_POST['nombre']);
+$email = trim($_POST['email']);
+$telefono = trim($_POST['telefono']);
+$mensaje = trim($_POST['mensaje']);
 
 
 // Import PHPMailer classes into the global namespace.
@@ -63,65 +63,74 @@ $bodyText =  "Correo de la web";
 
 // The HTML-formatted body of the email
 $bodyHtml = '
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1"/>
-  <meta name="x-apple-disable-message-reformatting"/>
-  <link rel="preconnect" href="https://fonts.googleapis.com"/>
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-  <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@600&display=swap" rel="stylesheet"/>
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta name="x-apple-disable-message-reformatting" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@600&display=swap" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap" rel="stylesheet">
   <title></title>
 </head>
 <body style="margin:0;padding:0;">
-  <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff; font-family: , sans-serif;">
+  <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
     <tr>
-      <td align="center" style="padding:0;">
-        <table role="presentation" style="width:602px;border-collapse:collapse;border:0px solid #cccccc;border-spacing:0;text-align:left; font-family: , sans-serif;">
+      <td style="padding:0;">
+        <table role="presentation" style="font-family: , sans-serif; width:602px;border-collapse:collapse;border:0px solid #cccccc;border-spacing:0;text-align:left;">
           <tr>
-            <td align="left" style="padding:10px 0 30px 0; font-family: , sans-serif;">
-              <img src="https://i.postimg.cc/VN79rVnD/logo-textylo-colores.gif" alt="TEXTYLO" width="200" style="height:auto;display:block; padding-top: 15px;" />
+            <td align="left" style="font-family: , sans-serif; padding:10px 0 30px 0;">
+            <img src="https://i.postimg.cc/VN79rVnD/logo-textylo-colores.gif" alt="TEXTYLO" width="200" style="height:auto;display:block; padding-top: 15px;" />
               <hr>
             </td>
           </tr>
           <tr>
-            <td style="padding:0px 30px 42px 20px; font-family: , sans-serif;">
-              <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0; font-family: , sans-serif;">
+            <td style="font-family: , sans-serif; padding:0px 30px 42px 20px;">
+              <table role="presentation" style="font-family: , sans-serif; width:100%;border-collapse:collapse;border:0;border-spacing:0;">
                 <tr>
-                  <td style="padding:0 0 36px 0;color:#153643; font-family: , sans-serif;">
-                    <img src="https://i.postimg.cc/k4Jn15MD/Nuevo-Contacto.png" alt="NUEVO CONTACTO" style="max-width: 500px; margin-left: 50px; margin-bottom: 60px; margin-top: 20px;"/>
+                  <td style="font-family: , sans-serif; padding:0 0 36px 0;color:#153643;">
+                    <h1 style="background: -webkit-linear-gradient(79.33deg,#810CCE 19.6%,#EE1796 80.4%);-webkit-text-fill-color: transparent;-webkit-background-clip: text;font-size: 3.5rem; text-align: center;">NUEVO CONTACTO</h1>
 
-                    <h4 style="margin:0 0 12px 0;font-size: 20px; text-align: center; font-family: , sans-serif;">Se ha capturado un nuevo lead en <b style="font-weight: 600; color: #599edf;">TEXTYLO</b></h4>
+                    <h4 style="margin:0 0 12px 0;font-size: 20px; text-align: center; font-family: , sans-serif;">Se ha capturado un nuevo lead en <b style="font-weight: 600; color: black;">TEXTYLO</b></h4>
 
-                    </td>
+                  </td>
                 </tr>
+                <center>
                 <tr>
-                  <td style="padding:0;">
-                    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0; font-family: , sans-serif;">
-                      <tr>
-                        <td style="width:260px;padding:0;vertical-align:top;color:#000000; font-family: , sans-serif;">
-                          <ul style="color: #aba9a8; list-style: none; text-align: center;">
-                              <li style="margin-bottom: 10px;">Nombre: '.$nombre.'</li>
-                              <li style="margin-bottom: 10px;">Correo electrónico:'.$correo.'</li>
-                              <li style="margin-bottom: 10px;">Teléfono: '.$telefono.'</li>
-                              <li style="margin-top: 40px;">Mensaje: '.$mensaje.'</li>
-                          </ul>
-                        </td>
+                      <td style="padding:0;">
+                        <table role="presentation" style="font-family: , sans-serif; width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                          <tr>
+                          <td style="padding:0;">
+                        <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0; font-family:, sans-serif;">
+                          <tr>
+                            <td style="width:260px;padding:0;vertical-align:top;color:#000000; font-family:, sans-serif;">
+                              <ul style="color: #aba9a8; list-style: none; text-align: center;">
+                                  <li style="margin-bottom: 10px;">Nombre: ' . $nombre . '</li>
+                                  <li style="margin-bottom: 10px;">Correo electrónico: ' . $email . '</li>
+                                  <li style="margin-bottom: 10px;">Teléfono:  ' . $telefono . '</li>
+                                  <li style="margin-top: 40px;">Mensaje: ' . $mensaje . '</li>
+                              </ul>
+                            </td>
+                          </tr>
+                        </table>
+                      </td>
                       </tr>
                     </table>
                   </td>
                 </tr>
+                </center>
               </table>
             </td>
           </tr>
           <tr>
-            <td style="padding:30px;background:#000000; font-family: , sans-serif;">
-              <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px; font-family: , sans-serif;">
+            <td style="padding:30px;background:#000000;">
+              <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;">
                 <tr>
-                  <td style="padding:0;width:50%; font-family: , sans-serif;" align="right">
-                    <img src="https://i.postimg.cc/4dpfLLNY/materialized-blueberry.gif" alt="MATERIALIZED BY Blueberry"/>
+                  <td style="padding:0;width:50%;" align="right">
+                    <img src="https://i.postimg.cc/4dpfLLNY/materialized-blueberry.gif" alt="MATERIALIZED BY Blueberry" />
                   </td>
                 </tr>
               </table>
@@ -132,43 +141,43 @@ $bodyHtml = '
     </tr>
   </table>
 </body>
+
 </html>
     
     ';
 
-$mail = new PHPMailer(true); 
+$mail = new PHPMailer(true);
 
 try {
-    // Specify the SMTP settings.
-    $mail->isSMTP();
-    $mail->setFrom($sender, $senderName);
-    $mail->Username   = $usernameSmtp;
-    $mail->From   = $usernameSmtp;
-    $mail->Password   = $passwordSmtp;
-    $mail->Host       = $host;
-    $mail->Port       = $port;
-    $mail->SMTPAuth   = true;
-    $mail->SMTPSecure = 'tls';
-    // $mail->SMTPDebug = true;
-    $mail->addCustomHeader('X-SES-CONFIGURATION-SET', $configurationSet);
+  // Specify the SMTP settings.
+  $mail->isSMTP();
+  $mail->setFrom($sender, $senderName);
+  $mail->Username   = $usernameSmtp;
+  $mail->From   = $usernameSmtp;
+  $mail->Password   = $passwordSmtp;
+  $mail->Host       = $host;
+  $mail->Port       = $port;
+  $mail->SMTPAuth   = true;
+  $mail->SMTPSecure = 'tls';
+  // $mail->SMTPDebug = true;
+  $mail->addCustomHeader('X-SES-CONFIGURATION-SET', $configurationSet);
 
-    // Specify the message recipients.
-    $mail->addAddress($recipient);
-    // You can also add CC, BCC, and additional To recipients here.
+  // Specify the message recipients.
+  $mail->addAddress($recipient);
+  // You can also add CC, BCC, and additional To recipients here.
 
-    // Specify the content of the message.
-    $mail->isHTML(true);
-    $mail->Subject    = $subject;
-    $mail->Body       = $bodyHtml;
-    $mail->AltBody    = $bodyText;
-    $mail->Send();
-    sleep(5);
-    header("Location: {$_SERVER['HTTP_REFERER']}");
-
+  // Specify the content of the message.
+  $mail->isHTML(true);
+  $mail->Subject    = $subject;
+  $mail->Body       = $bodyHtml;
+  $mail->AltBody    = $bodyText;
+  $mail->Send();
+  sleep(5);
+  header("Location: {$_SERVER['HTTP_REFERER']}");
 } catch (phpmailerException $e) {
-    echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
+  echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
 } catch (Exception $e) {
-    echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
+  echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
 }
 
 ?>
