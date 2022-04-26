58d2ed6f2a4dc4628869fe9ac85c07ee6b7519a6
BlueberryWeb <101658724+BlueberryWeb@users.noreply.github.com>
2022-04-26 08:43:11 -0500

    actualizacion correo

--- mail.php
+++ mail.php
@@ -63,60 +63,51 @@ $bodyText =  "Correo de la web";
 
 // The HTML-formatted body of the email
 $bodyHtml = '
-
-<!DOCTYPE html>
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1" />
-  <meta name="x-apple-disable-message-reformatting" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@600&display=swap" rel="stylesheet" />
+  <meta name="viewport" content="width=device-width,initial-scale=1"/>
+  <meta name="x-apple-disable-message-reformatting"/>
+  <link rel="preconnect" href="https://fonts.googleapis.com"/>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+  <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@600&display=swap" rel="stylesheet"/>
   <link href="https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap" rel="stylesheet">
   <title></title>
 </head>
 <body style="margin:0;padding:0;">
-  <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
+  <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff; font-family: , sans-serif;">
     <tr>
-      <td style="padding:0;">
-        <table role="presentation" style="font-family: , sans-serif; width:602px;border-collapse:collapse;border:0px solid #cccccc;border-spacing:0;text-align:left;">
+      <td align="center" style="padding:0;">
+        <table role="presentation" style="width:602px;border-collapse:collapse;border:0px solid #cccccc;border-spacing:0;text-align:left; font-family: , sans-serif;">
           <tr>
-            <td align="left" style="font-family: , sans-serif; padding:10px 0 30px 0;">
-              <img src="https://i.postimg.cc/vHX6QB70/Bueberrypostivo.jpg" alt=":Logo Blueberry" width="200" style="height:auto;display:block;" />
+            <td align="left" style="padding:10px 0 30px 0; font-family: , sans-serif;">
+              <img src="https://i.postimg.cc/VN79rVnD/logo-textylo-colores.gif" alt="TEXTYLO" width="200" style="height:auto;display:block; padding-top: 15px;" />
               <hr>
             </td>
           </tr>
           <tr>
-            <td style="font-family: , sans-serif; padding:0px 30px 42px 20px;">
-              <table role="presentation" style="font-family: , sans-serif; width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+            <td style="padding:0px 30px 42px 20px; font-family: , sans-serif;">
+              <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0; font-family: , sans-serif;">
                 <tr>
-                  <td style="font-family: , sans-serif; padding:0 0 36px 0;color:#153643;">
-                    <h1 style="background: -webkit-linear-gradient(79.33deg,#810CCE 19.6%,#EE1796 80.4%);-webkit-text-fill-color: transparent;-webkit-background-clip: text;font-size: 3.5rem; text-align: center;">NUEVO CONTACTO</h1>
+                  <td style="padding:0 0 36px 0;color:#153643; font-family: , sans-serif;">
+                    <img src="https://i.postimg.cc/k4Jn15MD/Nuevo-Contacto.png" alt="NUEVO CONTACTO" style="max-width: 500px; margin-left: 50px; margin-bottom: 60px; margin-top: 20px;"/>
 
-                    <h4 style="margin:0 0 12px 0;font-size: 20px; text-align: center; font-family: , sans-serif;">Se ha capturado un nuevo lead en <b style="font-weight: 600; color: black;">Blueberry</b></h4>
+                    <h4 style="margin:0 0 12px 0;font-size: 20px; text-align: center; font-family: , sans-serif;">Se ha capturado un nuevo lead en <b style="font-weight: 600; color: #599edf;">TEXTYLO</b></h4>
 
-                  </td>
+                    </td>
                 </tr>
                 <tr>
                   <td style="padding:0;">
-                    <table role="presentation" style="font-family: , sans-serif; width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0; font-family: , sans-serif;">
                       <tr>
-                        <td style="font-family:sans-serif; width:260px;padding:0;vertical-align:top;color:#000000;">
-                          <ul style="font-family:sans-serif; color: #aba9a8; list-style: none; text-align: center;">
-                            <li style="margin-bottom: 10px;">Nombre: '. $nombre .'</li>
-                            <li style="margin-bottom: 10px;">Correo electrónico: '. $email .'</li>
-                            <li style="margin-bottom: 10px;">Teléfono: '. $telefono .'</li>
-                            <li style="margin-top: 40px;">Mensaje: '. $mensaje .'</li>
+                        <td style="width:260px;padding:0;vertical-align:top;color:#000000; font-family: , sans-serif;">
+                          <ul style="color: #aba9a8; list-style: none; text-align: center;">
+                              <li style="margin-bottom: 10px;">Nombre: '.$nombre.'</li>
+                              <li style="margin-bottom: 10px;">Correo electrónico:'.$correo.'</li>
+                              <li style="margin-bottom: 10px;">Teléfono: '.$telefono.'</li>
+                              <li style="margin-top: 40px;">Mensaje: '.$mensaje.'</li>
                           </ul>
-                          <a href="https://www.facebook.com/blueberrymx/"><img src="https://i.postimg.cc/2ypVNcFm/FB-icono.png" alt="Blueberry facebook" style="margin-left: 140px; margin-right: 40px; margin-top: 80px; margin-bottom: 20px;" /></a>
-                          <a href="https://www.instagram.com/blueberrymx_/"><img src="https://i.postimg.cc/59HRPPRy/IG-icono.png" alt="Blueberry instagram" style="margin-right: 40px; margin-bottom: 20px;" /></a>
-                          <a href="https://www.youtube.com/channel/UCFsFEe_RNn4BeqG3Ce1I8rg"><img src="https://i.postimg.cc/bNkFPZ8j/YT-icono.png" alt="Blueberry youtube" style="margin-right: 40px; margin-bottom: 20px;" /></a>
-                          <a href="https://www.linkedin.com/company/blueberrymx"><img src="https://i.postimg.cc/HLsSb3Mp/IN-icono.png" alt="Blueberry linkedin" style="margin-right: 40px; margin-bottom: 20px;" /></a>
-                          <a href="https://vimeo.com/blueberrymx"><img src="https://i.postimg.cc/GtYzb5Wj/Vimeo-icono.png" alt="Blueberry Vimeo" style="margin-bottom: 20px;" /></a>
-                          <hr>
-                          <p style="color: #aba9a8; margin:20px 0 0px 0;font-size:12px;line-height:24px; text-align: center; font-family: , sans-serif;">Av. Rúben Darío #586 Col. Prados Providencia <br>
-                            C.P 44670, Guadalajara, Jalisco, México.</p>
                         </td>
                       </tr>
                     </table>
@@ -126,11 +117,11 @@ $bodyHtml = '
             </td>
           </tr>
           <tr>
-            <td style="padding:30px;background:#000000;">
-              <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;">
+            <td style="padding:30px;background:#000000; font-family: , sans-serif;">
+              <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px; font-family: , sans-serif;">
                 <tr>
-                  <td style="padding:0;width:50%;" align="right">
-                    <img src="https://i.postimg.cc/4dpfLLNY/materialized-blueberry.gif" alt="MATERIALIZED BY Blueberry" />
+                  <td style="padding:0;width:50%; font-family: , sans-serif;" align="right">
+                    <img src="https://i.postimg.cc/4dpfLLNY/materialized-blueberry.gif" alt="MATERIALIZED BY Blueberry"/>
                   </td>
                 </tr>
               </table>
@@ -141,7 +132,6 @@ $bodyHtml = '
     </tr>
   </table>
 </body>
-
 </html>
     
     ';
